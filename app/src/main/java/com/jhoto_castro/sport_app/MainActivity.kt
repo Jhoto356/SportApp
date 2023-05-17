dfee4ee56a6fd181ba7c033937d0fb66550bf0b5
jhonatan.arias <jhonatan.arias@onoff.com.co>
2023-05-17 17:49:41 -0500

    Create first OutlineTextField

--- app/src/main/java/com/jhoto_castro/sport_app/MainActivity.kt
+++ app/src/main/java/com/jhoto_castro/sport_app/MainActivity.kt
@@ -1,29 +1,33 @@
 package com.jhoto_castro.sport_app
 
-import android.content.Context
-import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.ScrollableState
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Card
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.core.content.ContextCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,22 +49,25 @@ fun ConstraintMain() {
         val (cardByFields, textVersion, imageLogin) = createRefs()
         Image(painter = painterResource(id = R.drawable.ic_logo_app),
             contentDescription = "",
-            modifier = Modifier.padding(24.dp).constrainAs(imageLogin) {
-            top.linkTo(parent.top)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-            bottom.linkTo(cardByFields.top)
-        })
+            modifier = Modifier
+                .padding(24.dp)
+                .constrainAs(imageLogin) {
+                    top.linkTo(parent.top)
+                    start.linkTo(cardByFields.start)
+                    end.linkTo(cardByFields.end)
+                    bottom.linkTo(cardByFields.top)
+                })
         Card(modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight(0.7F)
-            .padding(vertical = 8.dp, horizontal = 32.dp)
+            .padding(horizontal = 32.dp)
             .background(color = Color(R.color.bg_light))
             .constrainAs(cardByFields) {
                 bottom.linkTo(textVersion.top)
                 start.linkTo(parent.start)
                 end.linkTo(parent.end)
             }) {
+            ContentCard()
         }
         Text(
             text = stringResource(id = R.string.version_app),
@@ -78,8 +85,37 @@ fun ConstraintMain() {
         )
     }
 }
+@Composable
+fun SetLabelIniTextField(textToShow: String) {
+    Text(text = textToShow)
+}
 
 @Composable
 fun ContentCard() {
-
+    ConstraintLayout(
+        modifier = Modifier
+            .background(color = colorResource(id = R.color.white))
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        val (txtFieldUserName, textFieldPassword, btnLogIn) = createRefs()
+        var userName by remember { mutableStateOf(TextFieldValue()) }
+        OutlinedTextField(
+            TextFieldValue
+            value = userName,
+            onValueChange = { userName = it },
+            label = SetLabelIniTextField(textToShow = stringResource(id = R.string.label_text_user_name)),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            maxLines = 1,
+            selectionColor = colorResource(id = R.color.color_primary_dark),
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .constrainAs(txtFieldUserName) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        )
+    }
 }
